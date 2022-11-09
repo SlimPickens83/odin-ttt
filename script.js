394a00247774aa44601102f1f2de1190e1087985
Mike Radcliffe <mikerad83@gmail.com>
2022-11-09 15:14:53 -0500

    Re-patterned gaemboard function as a module.

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-const gameboard = document.getElementById("gameboard");
-const squares = 3;
+const board = ( function() {
+	'use strict';
+	return {
+		createBoard: function() {
+			const gameboard = document.getElementById("gameboard");
+			const boardArray = [];
+            const squares = 3;
+            let indexNum = 1;
 
+			for (let i = 0; i < squares; i++) {
+				const row = document.createElement("div");
+				row.classList.add("row");
+				gameboard.appendChild(row);
 
-for (let i = 0; i < squares; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    gameboard.appendChild(row);
+				for (let j = 0; j < squares; j++) {
+					const box = document.createElement("div");
+                    box.setAttribute('id', indexNum);        ;
+					box.classList.add("box", "gameText");
+					box.addEventListener("click", function(){
+						box.textContent = "X";
+					});
 
-    for (let i = 0; i < squares; i++) {
-        const box = document.createElement("div");        
-        box.classList.add("box", "gameText");
-        box.addEventListener("click", function(){
-            box.textContent = "X";
-        });
+					row.appendChild(box);
+                    
+                    boardArray.push(box);
 
-        row.appendChild(box);        
+                        console.log(`indexNum: ${indexNum}`);
+                    indexNum++;
+                        console.log(`indexNum after increment: ${indexNum}`);
 
-    }
-   
-}
+                        console.log(i);
+
+                }
+
+                if (i === 2) {
+                    console.log(boardArray);
+                }
+			}
+		}
+	};
+})();
+
+board.createBoard();
