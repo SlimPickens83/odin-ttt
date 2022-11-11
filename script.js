84583f8ebf7bdf457bdaba52941ee5369f0c5b89
Mike Radcliffe <mikerad83@gmail.com>
2022-11-11 10:48:07 -0500

    Continued to develop game code. Game not in a working state yet.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Player objects
+
+const player = (id, name, symbol) => {
+    return{id, name, symbol};
+ }
+ 
+const playerOne = player(1, "input1", 'X');
+const playerTwo = player(2, "input2", 'O');
+let livePlayer;
+
+
 // Gameboard module
 
 const board = ( function() {
@@ -16,10 +27,10 @@ const board = ( function() {
 
 				for (let j = 0; j < squares; j++) {
 					const box = document.createElement("div");
-                    box.setAttribute('id', indexNum);        ;
+                    box.setAttribute('id', indexNum);
 					box.classList.add("box", "gameText");
 					box.addEventListener("click", function(){
-						box.textContent = "X";
+						box.textContent = 'X';
 					});
 
 					row.appendChild(box);
@@ -42,15 +53,138 @@ const board = ( function() {
 	};
 })();
 
+
+// Gameplay module
+
+const round = ( function() {
+    'use strict';
+
+    function _rollInitiative () {
+        let ini = Math.random()*100;
+        let roll;
+
+        if (ini < 50) {
+            roll = 1;
+        } else if (ini >= 50) {
+            roll = 2;
+        }
+
+        return roll;
+
+    }    
+
+    function _winState (x) {
+        if (x.a.textContent === "X" && x.b.textContent === "X" && x.c.textContent === "X") {
+            return true;
+        } else if (x.a.textContent === "O" && x.b.textContent === "O" && x.c.textContent === "O") {
+            return true;
+        }
+    }
+
+    function _findOut () {
+        for (i = 0; i < 9; i++) {
+                  won = _winState(winners[i]);
+                  if (won === true) {
+                      console.log(`${winners[i]} is a winner`);
+                  }
+              }
+      }
+
+    function _isWinner () {
+        let i;
+
+        const winnersObject = (a, b, c) => {
+            return {a, b, c};
+        };
+        
+        const one = winnersObject(1, 2, 3);
+        const two = winnersObject(4, 5, 6);
+        const three = winnersObject(7, 8, 9);
+        const four = winnersObject(1, 4, 7);
+        const five = winnersObject(2, 5, 8);
+        const six = winnersObject(3, 6, 9);
+        const seven = winnersObject(1, 5, 9);
+        const eight = winnersObject(3, 5, 7);
+        
+        const winners = [one, two, three, four, five, six, seven, eight];
+        let won = false;
+
+        for (i = 0; i < 9; i++) {
+            won = _findOut(winners[i]);
+            if (won === true) {
+                break;
+            }
+        }
+
+        return won;
+
+    }
+
+    function _boardSpec() {
+        let k;
+        let turn = 0;
+
+        for (k = 1; k <= 9; k++) {
+            const box = document.getElementById(k);
+            let result = false;
+            let winningPlayer = "undecided";
+
+            box.addEventListener("click", function(){
+                box.textContent = livePlayer.symbol;
+
+                if (turn >= 3) {
+                    result = _isWinner();
+                        console.log(`result: ${result}`);
+                } else if (turn < 3 || result === false) {
+                        console.log(`result: ${result}`);
+                    turn++;
+                    return turn;
+                }
+                
+                if (result === true) {
+                    winningPlayer = livePlayer;
+                        console.log(`winningPlayer: ${winningPlayer}`);
+                    alert(`${winningPlayer} wins!`);
+                    board.createBoard();
+                }
+                           
+            });
+        }
+    }
+
+    return {
+        playerTurn: function() {
+            let turnCount = 1;
+            let playerSelect = _rollInitiative();
+
+            _boardSpec();
+            
+            if (turnCount === 1) {
+                if (playerSelect === 1) {
+                    livePlayer = playerOne;
+                } else if (playerSelect === 2) {
+                    livePlayer = playerTwo;
+                }
+
+                turnCount = _boardSpec();
+
+            } else if (livePlayer === playerOne) {   
+                livePlayer = playerTwo;
+
+                turnCount = _boardSpec();
+
+            } else if (livePlayer === playerTwo) {
+                livePlayer = playerOne;
+
+                turnCount = _boardSpec();
+
+            }
+        }
+    };
+})();
+
+
+// Play game
+
 board.createBoard();
-
-
-// Player objects
-
-const player = (id, name) => {
-   return{id, name};
-}
-
-const playerOne = player(1, "input1");
-const playerTwo = player(2, "input2");
-
+round.playerTurn();
