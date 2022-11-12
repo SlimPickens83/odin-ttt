2839a1c7e23756bff6ef789b47cf8360a7f6c5a3
Mike Radcliffe <mikerad83@gmail.com>
2022-11-12 18:20:29 -0500

    Refined process for inputting player names and for displaying turn information

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ const player = (id, name, symbol) => {
     return{id, name, symbol};
  }
  
-const playerOne = player("player1", "input1", 'X');
-const playerTwo = player("player2", "input2", 'O');
+const playerOne = player("player1", "input", 'X');
+const playerTwo = player("player2", "input", 'O');
+
+// Start button module
+
+// const startGame = ( function() {
+//     return {
+//         createPlayers: function() {
+//             const startButton = document.getElementById("game");
+//             const p1 = document.getElementById("p1");
+//             const p2 = document.getElementById("p2");
+
+//             startButton.addEventListener("click", function() {
+//                 playerOne.name = prompt("Player One, please enter your name:");
+//                 playerTwo.name = prompt("Player Two, please enter your name:");
+
+//                 p1.textContent = playerOne.name;
+//                 p2.textContent = playerTwo.name;
+
+//             });
+//         }
+//     }   
+// })();
 
 
 // Gameboard module
@@ -13,13 +34,14 @@ const playerTwo = player("player2", "input2", 'O');
 const board = ( function() {
 	'use strict';
 
+    const gameboard = document.getElementById("gameboard");
+    const button = document.getElementById("reset");
+	const boardArray = [];
+    const squares = 3;
+    let indexNum = 1;
+
 	return {
 		createBoard: function() {
-			const gameboard = document.getElementById("gameboard");
-			const boardArray = [];
-            const squares = 3;
-            let indexNum = 1;
-
 			for (let i = 0; i < squares; i++) {
 				const row = document.createElement("div");
 				row.classList.add("row");
@@ -38,7 +60,8 @@ const board = ( function() {
                 }
  
 			}
-		}
+
+		}     
 	};
 
 })();
@@ -49,22 +72,50 @@ const board = ( function() {
 const round = ( function() {
     'use strict';
 
+    const turn = document.getElementById("turn"); 
     let livePlayer = {};
-    let playerSelect;
     let turnCount = 1;
 
     function _rollInitiative () {
         let ini = Math.random()*100;
-        let roll;
+        let roll = {};
 
         if (ini < 50) {
-            roll = 1;
+            roll = playerOne;
         } else if (ini >= 50) {
-            roll = 2;
+            roll = playerTwo;
         }
 
         return roll;
 
+    }
+
+    function _startGame () {
+        const startButton = document.getElementById("game");
+        const p1 = document.getElementById("p1");
+        const p2 = document.getElementById("p2");      
+    
+        startButton.addEventListener("click", function() {
+            while (gameboard.firstChild) {
+                gameboard.removeChild(gameboard.firstChild);
+            }
+                
+            board.createBoard();
+
+            turn.textContent = "[        ]";
+
+            playerOne.name = prompt("Player One, please enter your name:");
+            playerTwo.name = prompt("Player Two, please enter your name:");
+    
+            p1.textContent = playerOne.name;
+            p2.textContent = playerTwo.name;
+
+            livePlayer = _rollInitiative();
+
+            turn.textContent = `[    ${livePlayer.name} goes first!    ]`;
+    
+        });
+      
     }
 
     function _isWinner () {
@@ -110,18 +161,20 @@ const round = ( function() {
 
     }
 
-    function _boardSpec(endGame) {        
+    function _boardSpec(victor) { 
 
         for (let l = 1; l <= 9; l++) {
-            const box = document.getElementById(l);
+            const box = document.getElementById(l);   
 
             if (box.textContent != 'X' && box.textContent != '0') {
                 box.addEventListener("click", function () {   
-                        _boxValue(box, endGame);
+                        _boxValue(box, victor);
+                        turn.textContent = `[    Turn ${turnCount}: ${livePlayer}    ]`;
                 });
 
             } else {
-                if (endGame === true) {
+                if (victor === true) {
+                    turn.textContent = `[    ${livePlayer} wins!    ]`;
                     alert("Game over. Please refresh and play again.");
                 } else {
                     box.addEventListener("click", function () {
@@ -137,36 +190,29 @@ const round = ( function() {
     return {
         playerTurn: function() {
             let victor = _isWinner();
+
             if (turnCount > 3) {
                 console.log(`_isWinner: ${_isWinner()}`);
                 console.log(`victor: ${victor}`);
+
             }
 
             if (turnCount === 1) {
-                playerSelect = _rollInitiative();
-            } else if (turnCount > 1) {
-                if (playerSelect === 1) {
-                    playerSelect = 2;
-                } else if (playerSelect === 2) {
-                    playerSelect = 1;
-                }
-            }
+                _startGame();
 
-            if (playerSelect === 1) {
-	            livePlayer = playerOne;
-            } else if (playerSelect === 2) {
-	            livePlayer = playerTwo;
-            }      
+            } else if (turnCount > 1) {
+                if (livePlayer === playerOne) {
+                    livePlayer = playerTwo;
+                } else if (livePlayer === playerTwo) {
+                    livePlayer = playerOne;
+                }
+
+            }            
 
             if (victor === true) {
                 alert(`${livePlayer.name} wins!`);
-                alert("Game over. Please refresh and play again.");
+                alert("Game over. Select 'Start New Game' to play again.");
 
-                // while (gameboard.firstChild) {
-                //     gameboard.removeChild(gameboard.firstChild);
-                //     }
-            
-                // board.createBoard();
             }
 
             for (let k = 1; k <= 9; k++) {
@@ -179,7 +225,7 @@ const round = ( function() {
     
             }
 
-            _boardSpec();            
+            _boardSpec(victor);            
            
         }
     };
